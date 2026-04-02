07c5474eb994bd8992a6ff01b6103356c9dddef2
DiatomStone <gnhat@pdx.edu>
2026-04-01 17:16:56 -0700

    In class update 2

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Name: Nhat Nguyen
 Course: ECE 510 Sprint 2026
 Project topic candidate: 
 
+
+
+
--- codefest/cf01/profiling/resnet18_profile.py
+++ codefest/cf01/profiling/resnet18_profile.py
@@ -6,4 +6,6 @@ from torchinfo import summary
 model = resnet18()
 
 # Profile model
-summary(model, input_size=(1, 3, 224, 224))
+summary(model, 
+        input_size=(1, 3, 224, 224),
+        col_names=["mult_adds", "num_params", "input_size", "output_size"])
