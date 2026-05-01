bd601d8dcdd9b320acf0462b32f23f46d26fe0cf
Nhat Nguyen <gnhat@pdx.edu>
2026-05-01 01:18:13 -0700

    update no comment

--- tools/deliverable_check.sh
+++ tools/deliverable_check.sh
@@ -1,10 +1,13 @@
 cd ..
 #cf04
-ls codefest/cf04/hdl/mac_llm_A.*
-ls codefest/cf04/hdl/mac_llm_B.*
-ls codefest/cf04/hdl/mac_tb.*
-ls codefest/cf04/hdl/mac_correct.*
-ls codefest/cf04/review/mac_code_review.md
-ls codefest/cf04/cman_quantization.md
-ls codefest/cf04/cocotb_mac/test_mac.py
+ls codefest/cf04/hdl/mac_llm_A.*> /dev/null
+ls codefest/cf04/hdl/mac_llm_B.*> /dev/null
+ls codefest/cf04/hdl/mac_tb.*> /dev/null
+ls codefest/cf04/hdl/mac_correct.*> /dev/null
+ls codefest/cf04/review/mac_code_review.md > /dev/null
+ls codefest/cf04/cman_quantization.md > /dev/null
+ls codefest/cf04/cocotb_mac/test_mac.py > /dev/null
+ls codefest/cf05/cman_systolic_trace.md > /dev/null
+
+ls codefest/errorcheck > /dev/null
 read -p "[Enter] to exit"
