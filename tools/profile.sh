a5bd84161341dbcf28f0122801d8d22327d06a93
DiatomStone <gnhat@pdx.edu>
2026-06-06 18:45:05 -0700

    sync measured 2 way fp32,fp64 conversion

--- tools/profile.sh
+++ tools/profile.sh
@@ -1,10 +1,12 @@
+#!/bin/bash
 app="train.py"
-output="../../codefest/cf02/profiling/project_profile_staging.txt"
+output="project_profile.txt"
+input_dir="../project/m4/orginal_software"
 
 cli_loop(){
 for ((i=0; i<$1; i++)) do
 	python3 -m cProfile -s cumtime "$app" --steps 500 --config $2 |\
-	head -n 40 |\
+	grep cast_fp32_roundtrip |\
 	tee -a "$output"
 done
 }
@@ -12,9 +14,10 @@ done
 #main 
 source pytorch.sh
 mkdir -p "$(dirname "$output")"
-cd ../project/transformer_lm
+cd $input_dir
+> $output
 echo "------------[ 2x config=medium ]--------------" >> $output
-cli_loop "2" "small"
+cli_loop "10" "small"
 
 read -p "[Enter] to close"
 
